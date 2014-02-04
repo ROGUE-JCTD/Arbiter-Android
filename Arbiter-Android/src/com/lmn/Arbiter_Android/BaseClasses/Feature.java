ROGUE-JCTD/Arbiter-Android

commit ac3c5d8b058266662d034d903990a01680c2f57e

Make sure that if a feature has already been synced, don’t mark it for upload

Arbiter-Android/src/com/lmn/Arbiter_Android/Dialog/Dialogs/FeatureDialog/FeatureDialogHelper.java
@@ -275,6 +275,7 @@ public class FeatureDialogHelper {
 			insertedNewFeature = true;
 		}else{
 			if(feature.getSyncState().equals(FeaturesHelper.SYNC_STATES.SYNCED)){
+				feature.setSyncState(FeaturesHelper.SYNC_STATES.NOT_SYNCED);
 				feature.setModifiedState(FeaturesHelper.MODIFIED_STATES.MODIFIED);
 			}
 			

Arbiter-Android/assets/www/js/Arbiter/Loaders/FeaturesLoader.js
@@ -58,6 +58,14 @@ Arbiter.Loaders.FeaturesLoader = (function(){
 	};
 	
 	var setState = function(olFeature){
+		
+		var syncState = olFeature.metadata[Arbiter.FeatureTableHelper.SYNC_STATE];
+		
+		// If the feature is already synced, don't mark the feature for uploading
+		if(syncState === Arbiter.FeatureTableHelper.SYNC_STATES.SYNCED){
+			return;
+		}
+		
 		var state = olFeature.metadata[Arbiter.FeatureTableHelper.MODIFIED_STATE];
 		
 		if(state === Arbiter.FeatureTableHelper.MODIFIED_STATES.DELETED){

commit 10b21b6af8a1f15b2ea97f4f955f4e1ec340efa3

Fixed a bug with deleting features caused by commit ac3c5d8b058266662d034d903990a01680c2f57e

Arbiter-Android/src/com/lmn/Arbiter_Android/BaseClasses/Feature.java
@@ -85,6 +85,10 @@ public class Feature {
 		return attributes.get(FeaturesHelper.SYNC_STATE);
 	}
 	
+	public String getFID(){
+		return attributes.get(FeaturesHelper.FID);
+	}
+	
 	public void setSyncState(String state){
 		attributes.put(FeaturesHelper.SYNC_STATE, state);;
 	}

Arbiter-Android/src/com/lmn/Arbiter_Android/Dialog/Dialogs/FeatureDialog/FeatureDialogHelper.java
@@ -454,15 +454,27 @@ public class FeatureDialogHelper {
 		
 		if(feature.getSyncState().equals(FeaturesHelper.SYNC_STATES.SYNCED)){
 			feature.setModifiedState(FeaturesHelper.MODIFIED_STATES.DELETED);
+			feature.setSyncState(FeaturesHelper.SYNC_STATES.NOT_SYNCED);
 			
 			FeaturesHelper.getHelper().update(db, 
 					feature.getFeatureType(), feature.getId(),
 					feature);
 		}else{ 
-			// If the feature isn't synced, then we don't
-			// need to keep track of it anymore.
-			FeaturesHelper.getHelper().delete(db,
-					feature.getFeatureType(), feature.getId());
+			
+			feature.setModifiedState(FeaturesHelper.MODIFIED_STATES.DELETED);
+			
+			String fid = feature.getFID();
+			
+			if(fid != null && !fid.equals("")){
+				FeaturesHelper.getHelper().update(db, 
+						feature.getFeatureType(), feature.getId(),
+						feature);
+			}else{
+				// If the feature isn't synced and it's not on the server, 
+				// then we don't need to keep track of it anymore.
+				FeaturesHelper.getHelper().delete(db,
+						feature.getFeatureType(), feature.getId());
+			}
 		}
 		
 		ControlPanelHelper cpHelper = new ControlPanelHelper(activity);